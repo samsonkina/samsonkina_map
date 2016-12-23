49342d5529dd3c0ad45cbc7305edf0b00cf48911
samsonkina <kumome@gmail.com>
2016-12-23 16:18:18 +0300

    окно выбора, конвертация
    изменен внешний вид окна, добавлен класс для перевода координат

---
+++ Convert.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace samsonkina_map
+{
+    public class Convert
+    {
+        //долгота
+        double longitude; 
+        //широта
+        double latitude;
+        //зум
+        int zoom;
+
+
+        public Convert(double lon, double lat, int z)
+        {
+            longitude = lon;
+            latitude = lat;
+            zoom = z;
+        }
+
+        // перевод координат тайла в мировые координаты
+        public PointF TileToWorldPos()
+        {
+            PointF p = new Point();
+            float n = (float)(Math.PI - ((2.0 * Math.PI * latitude) / Math.Pow(2.0, zoom)));
+
+            p.X = (float)((longitude / Math.Pow(2.0, zoom) * 360.0) - 180.0);
+            p.Y = (float)(180.0 / Math.PI * Math.Atan(Math.Sinh(n)));
+
+            return p;
+        }
+
+        // перевод мировых координат в координаты тайла
+        public Point WorldToTilePosSpheroid()
+        {
+            Point p = new Point();
+            p.X = (int)(((longitude + 180.0) / 360.0 * (1 << zoom)));
+            p.Y = (int)((1.0 - Math.Log(Math.Tan(latitude * Math.PI / 180.0) +
+                1.0 / Math.Cos(latitude * Math.PI / 180.0)) / Math.PI) / 2.0 * (1 << zoom));
+
+            return p;
+        }
+
+
+
+    }
+}
---
+++ DownloadWindow.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace samsonkina_map
+{
+    partial class DownloadWindow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.ListOfServers = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ListOfCities = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(197, 180);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 14;
+            this.btnLoad.Text = "Загрузить";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // ListOfServers
+            // 
+            this.ListOfServers.FormattingEnabled = true;
+            this.ListOfServers.Location = new System.Drawing.Point(12, 129);
+            this.ListOfServers.Name = "ListOfServers";
+            this.ListOfServers.Size = new System.Drawing.Size(260, 21);
+            this.ListOfServers.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Список серверов";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Список городов";
+            // 
+            // ListOfCities
+            // 
+            this.ListOfCities.FormattingEnabled = true;
+            this.ListOfCities.Location = new System.Drawing.Point(12, 77);
+            this.ListOfCities.Name = "ListOfCities";
+            this.ListOfCities.Size = new System.Drawing.Size(260, 21);
+            this.ListOfCities.TabIndex = 10;
+            // 
+            // DownloadWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.ListOfServers);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ListOfCities);
+            this.Name = "DownloadWindow";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        public System.Windows.Forms.ComboBox ListOfServers;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.ComboBox ListOfCities;
+    }
+}
+
