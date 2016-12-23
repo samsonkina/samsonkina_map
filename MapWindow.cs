04527d15c7278dc13227294855df2c66a8a94fa7
samsonkina <kumome@gmail.com>
2016-12-23 19:43:51 +0300

    окно карты
    добавлено окно для отрисовки карты, создан класс для отрисовки панель в окне карты

---
+++ MapWindow.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace samsonkina_map
+{
+    public partial class MapWindow : Form
+    {
+        int zoom = 15;
+        Point tile = new Point();
+        public MapWindow()
+        {
+            Convert ToConvert = new Convert(DownloadWindow.SelectedCity.longitude, DownloadWindow.SelectedCity.latitude, zoom);
+            tile = ToConvert.WorldToTilePosSpheroid();
+            InitializeComponent();
+        }
+
+
+      
+
+        private void panel_MouseUp(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void panel_MouseMove(object sender, MouseEventArgs e)
+        {
+           
+        }
+
+        private void panel_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void panel_MouseWheel(object sender, MouseEventArgs e)
+        {
+           
+
+        }
+
+        private void panel_MouseEnter(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
---
+++ Panel1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace samsonkina_map
+{
+    class Panel1 : Panel
+    {
+        public Panel1()
+        {
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.UserPaint, true);
+        }
+    }
+}
